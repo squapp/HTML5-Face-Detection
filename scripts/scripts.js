f8977658a3baa257b53b1eb0fb4ddc3507917c5d
sph <stefan.pham@fokus.fraunhofer.de>
2012-09-20 14:41:41 +0200

    veedlr

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,36 +5,74 @@ var		video = document.querySelector('video'),
 		canvas = document.getElementById("output"),
 		ctx = canvas.getContext("2d");
 		glasses.src = "i/glasses.png";
+		
+var data = new Array();
+		
 
 function html5glasses() {
 	// Start the clock 
 	var elapsed_time = (new Date()).getTime();
 
+	var timestamp = Math.round(video.currentTime*1000);
 	// Draw the video to canvas
-	ctx.drawImage(video, 0, 0, video.width, video.height, 0, 0, canvas.width, canvas.height);
+	//console.log(video.videoWidth + " " +  video.videoHeight + " " + canvas.width + " " + canvas.height);
+	ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, canvas.width, canvas.height);
+	
 
 	// use the face detection library to find the face
 	var comp = ccv.detect_objects({ "canvas" : (ccv.pre(canvas)),
 									"cascade" : cascade,
-									"interval" : 5,
+									"interval" : 4,
 									"min_neighbors" : 1 });
+									
 
 	// Stop the clock
 	time_dump.innerHTML = "Process time : " + ((new Date()).getTime() - elapsed_time).toString() + "ms";
 
 	// Draw glasses on everyone!
 	for (var i = 0; i < comp.length; i++) {
+
+		saveData(i,timestamp,comp[i].x/canvas.width,comp[i].y/canvas.height,comp[i].width/canvas.width,comp[i].height/canvas.height);
 		ctx.drawImage(glasses, comp[i].x, comp[i].y,comp[i].width, comp[i].height);
 	}
 }
 
+function saveData(objID, timestamp,x,y,xd,yd) {
+		console.log(objID + " " + timestamp + " " + x + " " + y + " " + xd + " "+ yd);
+		
+		if (data[objID] == null){
+			var newObject = {
+				"Name" : "Neues Objekt"+(objID+1),
+				"KeyFrames" : new Array()
+			};
+			data[objID] = newObject;
+			console.log(data[objID]);
+		} 
+		var newTimestamp = {
+			"Time" : timestamp,
+			"Point" : [
+				{"X":x,"Y":y},
+				{"X":x+xd,"Y":y},
+				{"X":x+xd,"Y":y+yd},
+				{"X":x,"Y":y+yd}
+			]
+		};
+		data[objID].KeyFrames.push(newTimestamp);
+			
+		
+}
+		
 /* Events */ 
 
 video.addEventListener('play', function() {
-	vidInterval = setInterval(html5glasses,200); 
+	video.playbackRate = 5;
+	video.style.display = "none";
+	canvas.style.display = "none";
+	vidInterval = setInterval(html5glasses, 200); 
 });
 
 video.addEventListener('ended', function() {
 	clearInterval(vidInterval);
 	time_dump.innerHTML = "finished";
+	console.log(data);
 });
